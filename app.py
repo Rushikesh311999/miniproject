75716497112dfa601743217402f5487b4ae8da67
Rushikesh311999 <55188535+Rushikesh311999@users.noreply.github.com>
2019-09-12 22:55:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask,render_template
-import matplotlib
-import numpy
 
+
+
+from flask import Flask, render_template, request
 app = Flask(__name__)
-
-@app.route('/a')
-def index():
+@app.route('/Input', methods=['post', 'get'])
+def login():
+    message = ''
+    if request.method == 'POST':
+        Source = request.form.get('src')  # access the data inside 
+        Destination = request.form.get('dest')
+        Budget = request.form.get('bgt')
  
-   
-   return render_template('hello.html',res="a")
+        message = "Enter details"
+ 
+    return render_template('page1.html', message=message)
 
 if __name__ == '__main__':
    app.run(debug = True)
