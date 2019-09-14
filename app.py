5d6e4dade95d451dedf33756efb01a07e14f8118
Rushikesh311999 <55188535+Rushikesh311999@users.noreply.github.com>
2019-09-14 14:31:16 +0530

    updated files

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-
-
-
-from flask import Flask, render_template, request
+from flask import Flask, redirect, url_for, request, render_template, abort, jsonify
 app = Flask(__name__)
-@app.route('/Input', methods=['post', 'get'])
-def login():
-    message = ''
+# serving form request from website
+@app.route('/send_data/', methods=['POST'])
+def send_data():
     if request.method == 'POST':
-        Source = request.form.get('src')  # access the data inside 
-        Destination = request.form.get('dest')
-        Budget = request.form.get('bgt')
- 
-        message = "Enter details"
- 
-    return render_template('page1.html', message=message)
+        #set up connection and send data to server
+        msg="successful"
+        return redirect(url_for('main_page', message=msg))
+
+@app.route('/main_page/<message>/')
+def main_page(message):
+    return render_template('page2.html')
+
+@app.route('/start_trip/')
+def start_trip():
+       return render_template('page1.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
